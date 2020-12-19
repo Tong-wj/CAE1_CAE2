967a2c9c4bad8936709d8695c94bfdc4de88b939
Tongwanjie <73418240+Tong-wj@users.noreply.github.com>
2020-12-19 23:06:26 +0800

    Add files via upload

---
+++ CAE1/models/cae_32x32x32_noise.py
@@ -0,0 +1,218 @@
+import torch
+import torch.nn as nn
+
+class Reshape(nn.Module):
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+    def forward(self, x):
+        return x.view((x.size(0),)+self.shape)
+        # return x.view(self.shape)
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class GaussianNoise(nn.Module):
+    """Gaussian noise regularizer.
+
+    Args:
+        sigma (float, optional): relative standard deviation used to generate the
+            noise. Relative means that it will be multiplied by the magnitude of
+            the value your are adding the noise to. This means that sigma can be
+            the same regardless of the scale of the vector.
+        is_relative_detach (bool, optional): whether to detach the variable before
+            computing the scale of the noise. If `False` then the scale of the noise
+            won't be seen as a constant but something to optimize: this will bias the
+            network to generate vectors with smaller values.
+    """
+
+    def __init__(self, snr=30, sigma=0.01, is_relative_detach=False):
+        super().__init__()
+        self.snr = snr
+        self.sigma = sigma
+        self.is_relative_detach = is_relative_detach
+        self.noise = torch.FloatTensor(32768,).to("cuda")
+
+    def forward(self, x):
+        snr = 10**(self.snr/10.0)
+        xpower = torch.sum(x**2)/32768.
+        npower = xpower/snr
+        noise = self.noise.normal_()*torch.sqrt(npower)
+        x = x + noise
+        # if self.training and self.sigma != 0:
+        #     scale = self.sigma * x.detach() if self.is_relative_detach else self.sigma * x
+        #     sampled_noise = self.noise.normal_() * scale
+        #     x = x + sampled_noise
+        return x 
+
+
+class CAE(nn.Module):
+    """
+    This AE module will be fed 3x128x128 patches from the original image
+    Shapes are (batch_size, channels, height, width)
+
+    Latent representation: 32x32x32 bits per patch => 240KB per image (for 720p)
+    """
+
+    def __init__(self):
+        super(CAE, self).__init__()
+
+        self.encoded = None
+
+        # ENCODER
+
+        # 64x64x64
+        self.e_conv_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 2, 1, 2)),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(5, 5), stride=(2, 2)),
+            nn.LeakyReLU()
+        )
+
+        # 128x32x32
+        self.e_conv_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 2, 1, 2)),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5, 5), stride=(2, 2)),
+            nn.LeakyReLU()
+        )
+
+        # 128x32x32
+        self.e_block_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x32x32
+        self.e_block_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x32x32
+        self.e_block_3 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 32x32x32
+        self.e_conv_3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
+            nn.Tanh()
+        )
+
+        self.channel = nn.Sequential(
+            Flatten(),
+            GaussianNoise(),
+            Reshape(32, 32, 32)
+        )
+
+        # DECODER
+
+        # 128x64x64
+        self.d_up_conv_1 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        # 128x64x64
+        self.d_block_1 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x64x64
+        self.d_block_2 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 128x64x64
+        self.d_block_3 = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+        )
+
+        # 256x128x128
+        self.d_up_conv_2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(in_channels=32, out_channels=256, kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        # 3x128x128
+        self.d_up_conv_3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=16, kernel_size=(3, 3), stride=(1, 1)),
+            nn.LeakyReLU(),
+
+            nn.ReflectionPad2d((2, 2, 2, 2)),
+            nn.Conv2d(in_channels=16, out_channels=3, kernel_size=(3, 3), stride=(1, 1)),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        ec1 = self.e_conv_1(x)
+        ec2 = self.e_conv_2(ec1)
+        eblock1 = self.e_block_1(ec2) + ec2
+        eblock2 = self.e_block_2(eblock1) + eblock1
+        eblock3 = self.e_block_3(eblock2) + eblock2
+        ec_3 = self.e_conv_3(eblock3)  # in [-1, 1] from tanh activation
+        ec3 = self.channel(ec_3)
+
+
+        # stochastic binarization
+        with torch.no_grad():
+            rand = torch.rand(ec3.shape).cuda()
+            prob = (1 + ec3) / 2
+            eps = torch.zeros(ec3.shape).cuda()
+            eps[rand <= prob] = (1 - ec3)[rand <= prob]
+            eps[rand > prob] = (-ec3 - 1)[rand > prob]
+
+        # encoded tensor
+        self.encoded = 0.5 * (ec3 + eps + 1)  # (-1|1) -> (0|1)
+
+        return self.decode(self.encoded)
+
+    def decode(self, encoded):
+        y = encoded * 2.0 - 1  # (0|1) -> (-1|1)
+
+        uc1 = self.d_up_conv_1(y)
+        dblock1 = self.d_block_1(uc1) + uc1
+        dblock2 = self.d_block_2(dblock1) + dblock1
+        dblock3 = self.d_block_3(dblock2) + dblock2
+        uc2 = self.d_up_conv_2(dblock3)
+        dec = self.d_up_conv_3(uc2)
+
+        return dec
